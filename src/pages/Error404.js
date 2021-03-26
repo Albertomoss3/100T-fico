5875d194cfdf897df9aa6b6be3f717202d10d798
Alberto <albertomoss3@gmail.com>
2021-03-26 15:04:10 -0600

    100tifico create

---
+++ src/pages/Error404.js
@@ -0,0 +1,9 @@
+const Error404 = () => {
+    const viewError404 = `
+        <div class="error404">
+            <h2>La pagina esta sufriendo amsiedad, por favor vuleva intentarlo</h2>
+        </div>
+    `;
+    return viewError404;
+} 
+export default Error404;
---
+++ src/template/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    const viewHeader = `
+        <div class="header-main">
+            <div class="header-logo">
+                <h1>
+                    <a href="/">
+                        100Tífico
+                    </a>
+                </h1>
+            </div>
+            <div class="header-nav">
+                <a href="#/about/">
+                    About
+                </a>
+            </div>
+        </div>
+    `;
+    return viewHeader;
+}
+
+export default Header;
